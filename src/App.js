9b9a50e69ee7ef7fa7db1d4d140c19702b35a1d4
Dauphine Burns <dauphineburns@gmail.com>
2021-09-11 08:18:25 -0400

    added app.js content from other port

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import Header from './components/header/header';
+import Projects from './components/projects/projects';
+import project from './project.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    project
+  };
+
+  render() {
+    return (
+      <>
+      <Header />
+      {this.state.project.map(projects => (
+        <Projects 
+        id={projects.id}
+        mockup={projects.mockup}
+        image={projects.image}
+        alt={projects.alt}
+        overview={projects.overview}
+        deployed={projects.deployed}
+        github={projects.github}
+        />
+      ))}
+      </>
+    )
+  }
 }
 
 export default App;
