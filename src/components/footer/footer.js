5f9b0bba51f668b19edcc850ffae4aaa1cfbd608
Dauphine Burns <dauphineburns@gmail.com>
2021-09-14 03:36:30 -0500

    footer js added

---
+++ src/components/footer/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./footer.css";
+
+function Footer () {
+    return (
+        <div class="footer container">
+            <ul>
+                <a id="numberf" href="#about">210-722-1739 |</a>
+                <a id="emailf" href="#about">dauphineburns@gmail.com |</a>
+                <a href="https://www.linkedin.com/in/dauphine-burns-b2b289134/" id="linkedinf">LinkedIn |</a>
+                <a href="https://github.com/burnsgirl"  id="githubf">GitHub</a>
+            </ul>
+        </div>
+    )
+}
+
+export default Footer;
