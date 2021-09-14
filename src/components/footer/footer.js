a089509056de89b30298fb13551778c302d871fc
Dauphine Burns <dauphineburns@gmail.com>
2021-09-14 03:42:39 -0500

    fixed card layout 850

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,9 +5,9 @@ function Footer () {
     return (
         <div class="footer container">
             <ul>
-                <a id="numberf" href="#about">210-722-1739 |</a>
-                <a id="emailf" href="#about">dauphineburns@gmail.com |</a>
-                <a href="https://www.linkedin.com/in/dauphine-burns-b2b289134/" id="linkedinf">LinkedIn |</a>
+                <a id="numberf" href="#about">210-722-1739</a>
+                <a id="emailf" href="#about">dauphineburns@gmail.com</a>
+                <a href="https://www.linkedin.com/in/dauphine-burns-b2b289134/" id="linkedinf">LinkedIn</a>
                 <a href="https://github.com/burnsgirl"  id="githubf">GitHub</a>
             </ul>
         </div>
