7280812625f75a201d00284b8ff3e08f6f1fa661
Dauphine Burns <dauphineburns@gmail.com>
2021-09-16 08:59:37 -0500

    footer 865 finished

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,10 +5,10 @@ function Footer () {
     return (
         <div class="footer container">
             <ul>
-                <a id="numberf" href="#about">210-722-1739</a>
-                <a id="emailf" href="#about">dauphineburns@gmail.com</a>
-                <a href="https://www.linkedin.com/in/dauphine-burns-b2b289134/" id="linkedinf">LinkedIn</a>
-                <a href="https://github.com/burnsgirl"  id="githubf">GitHub</a>
+                <a id="numberf" href="#about">210-722-1739</a><br/>
+                <a id="emailf" href="#about">dauphineburns@gmail.com</a><br/>
+                <a href="https://www.linkedin.com/in/dauphine-burns-b2b289134/" id="linkedinf">LinkedIn</a><br/>
+                <a href="https://github.com/burnsgirl"  id="githubf">GitHub</a><br/>
             </ul>
         </div>
     )
